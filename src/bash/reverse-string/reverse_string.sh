8bcbec702575849ce5614b90e805977c6b795eb7
bb4L <39266013+bb4L@users.noreply.github.com>
2020-09-06 10:53:22 +0200

    Add solution reverse-string

--- src/bash/reverse-string/reverse_string.sh
+++ src/bash/reverse-string/reverse_string.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-set -o noglob
-
 result=""
-
-for (( i=${#1}; i>=0; i--))
+for (( i=${#1}; i>0; i--))
 do
-    result=$result${1:$i:1}    
+    result+=${1:i-1:1}    
 done
 
-echo $result
+echo "$result"
